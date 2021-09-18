98c80bb4deba510e99ebce5227c0a2fede5bab00
Magnus Halvorsen <magnus.halvorsen@kdi.kongsberg.com>
2021-09-18 15:55:23 +0200

    Add simulated gravity

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+mod geometry;
+
 fn main() {
-    println!("Hello, world!");
+    let mut position = geometry::gen_point(0.0_f64, 1.0_f64, 0.0_f64);
+    let mut velocity = geometry::gen_vector(1.0_f64, 1.0_f64, 1.0_f64).normalize();
+
+    let gravity = geometry::gen_vector(0.0_f64, -0.1_f64, 0.0_f64);
+    let wind = geometry::gen_vector(-0.01_f64, 0.0_f64, 0.0_f64);
+
+    while position.y > 0_f64 {
+        let (new_position, new_velocity) = tick(position, velocity, gravity, wind);
+        position = new_position;
+        velocity = new_velocity;
+
+        println!("{0}", position)
+    }
+}
+
+fn tick(position: geometry::Point, velocity: geometry::Vector, gravity: geometry::Vector, wind: geometry::Vector) -> (geometry::Point, geometry::Vector) {
+    let resulting_position = position + velocity;
+    let resulting_velocity = velocity + gravity + wind;
+    (resulting_position, resulting_velocity)
 }
 
 
