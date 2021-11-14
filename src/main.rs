0ce21c2038dc76d996d05a78a967a5b2eb308545
Magnus Halvorsen <magnushalvorsen1991@gmail.com>
2021-11-14 15:02:10 +0100

    First scene wip

--- src/main.rs
+++ src/main.rs
@@ -41,39 +41,21 @@ fn main() {
     let mut nof_misses = 0;
     let mut nof_hits = 0;
 
-    let mut ray = Ray {
-        origin: Tuple::point(0_f32, 0_f32, -5_f32),
-        direction: Tuple::vector(0_f32, 0_f32, 1_f32)
-    };
-    for y in 0..DIM {
-        let world_y = half - (pixel_size * (y as f32));
-        for x in 0..DIM {
-            let world_x = -half + ((pixel_size) * (x as f32));
+    let mut floor = Sphere::new(1);
+    floor.set_transformation(Matrix4::identity().scale(10_f32, 0.01_f32, 10_f32));
+    floor.material.color = Color::new(1_f32, 0.9_f32, 0.9_f32);
+    floor.material.specular = 0_f32;
+    let left_wall = Sphere::new(2);
+    let right_wall = Sphere::new(3);
+    
+    let middle = Sphere::new(4);
+    let right = Sphere::new(5);
+    let left = Sphere::new(6);
 
-            ray.direction = (Tuple::vector(world_x, world_y, wall_z) - ray.origin).normalize();
-
-            let intersections = intersects(&sphere, &ray);
-
-            if intersections.len() > 0 {
-                let hit = &intersections[0];
-                let hit_point = ray.position(hit.t);
-                let normal = sphere.normal_at(&hit_point);
-                let eye = -ray.direction;
-
-                let color = lighting(&sphere.material, &light, &ray.origin, &eye, &normal);
-                canvas::set_pixel(&mut canvas, x, y, color);
-                nof_hits = nof_hits + 1;
-            }
-            else {
-                nof_misses = nof_misses + 1;                
-            }
-        }   
-        
-    }
 
     println!("Nof hits: {}. Nof misses: {}.", nof_hits, nof_misses);
 
-    canvas::canvas_to_file(&canvas, "test.ppm".to_string());
+    /* canvas::canvas_to_file(&canvas, "test.ppm".to_string());
 
     let mut buffer: Vec<u32> = vec![0; DIM * DIM];
 
@@ -107,7 +89,7 @@ fn main() {
         window
             .update_with_buffer(&buffer, DIM, DIM)
             .unwrap();
-    }
+    } */
 }
 
 fn limit(value: i32, min: u8, max: u8) -> u8 {
